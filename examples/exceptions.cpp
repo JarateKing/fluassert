8f2479d84ffb5173fe097f02780c142b9adfc150
JarateKing <scmacintosh@upei.ca>
2020-12-24 13:21:03 -0400

    added multi-argument example

--- examples/exceptions.cpp
+++ examples/exceptions.cpp
@@ -1,6 +1,13 @@
+#define FLUASSERT_NOABORT
 #include "../src/fluassert.h"
 
 int main() {
+	// zero-argument function
 	auto lambda = [](){ throw "exception"; };
-	fluassert(lambda, should.Not.throw_error);
+	fluassert(lambda, should.throw_error);
+	
+	// multiple-argument function, wrapped in a lambda
+	auto lambda2 = [](std::vector<int> a, int b){ return a.at(b); };
+	std::vector<int> arr(0);
+	fluassert([&](){ return lambda2(arr, 100); }, should.throw_error);
 }
