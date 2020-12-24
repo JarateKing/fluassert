a7d74b053725dda594fc723ca75557784b8ece6b
JarateKing <scmacintosh@upei.ca>
2020-12-24 17:37:16 -0400

    added embedded lambda example

--- examples/exceptions.cpp
+++ examples/exceptions.cpp
@@ -2,12 +2,15 @@
 #include "../src/fluassert.h"
 
 int main() {
-	// zero-argument function
+	// zero-argument lambda
 	auto lambda = [](){ throw "exception"; };
 	fluassert(lambda, should.throw_error);
 	
-	// multiple-argument function, wrapped in a lambda
-	auto lambda2 = [](std::vector<int> a, int b){ return a.at(b); };
+	// zero-argument lambda directly inside fluassert
+	fluassert([](){ throw "exception"; }, should.throw_error);
+	
+	// multiple-argument lambda, wrapped in a zero-argument lambda
 	std::vector<int> arr(0);
+	auto lambda2 = [](std::vector<int> a, int b){ return a.at(b); };
 	fluassert([&](){ return lambda2(arr, 100); }, should.throw_error);
 }
