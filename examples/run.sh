3ff4eaba6712681151969359cd6798897c53f8c5
JarateKing <scmacintosh@upei.ca>
2020-12-30 13:58:14 -0400

    made run and run_all different

--- examples/run.sh
+++ examples/run.sh
@@ -1,44 +1,4 @@
-echo BASIC.CPP
 rm a.exe
-g++ -g -O2 -std=gnu++17 -static basic.cpp
-./a.exe
-
-echo FAIL.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static fail.cpp
-./a.exe
-
-echo FAIL_MULTIPLE.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static fail_multiple.cpp
-./a.exe
-
-echo NDEBUG.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static ndebug.cpp
-./a.exe
-
-echo CUSTOM_HANDLER.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static custom_handler.cpp
-./a.exe
-
-echo EXCEPTIONS.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static exceptions.cpp
-./a.exe
-
-echo WITH_ASSERT.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static with_assert.cpp
-./a.exe
-
-echo WITHOUT_ASSERT.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static without_assert.cpp
-./a.exe
-
-echo CUSTOM_ASSERT.CPP
-rm a.exe
-g++ -g -O2 -std=gnu++17 -static custom_assert.cpp
+echo $1
+g++ -g -O2 -std=gnu++17 -static $1
 ./a.exe
---
+++ examples/run_all.sh
@@ -0,0 +1,26 @@
+echo BASIC.CPP
+./run.sh basic.cpp
+
+echo FAIL.CPP
+./run.sh fail.cpp
+
+echo FAIL_MULTIPLE.CPP
+./run.sh fail_multiple.cpp
+
+echo NDEBUG.CPP
+./run.sh ndebug.cpp
+
+echo CUSTOM_HANDLER.CPP
+./run.sh custom_handler.cpp
+
+echo EXCEPTIONS.CPP
+./run.sh exceptions.cpp
+
+echo WITH_ASSERT.CPP
+./run.sh with_assert.cpp
+
+echo WITHOUT_ASSERT.CPP
+./run.sh without_assert.cpp
+
+echo CUSTOM_ASSERT.CPP
+./run.sh custom_assert.cpp
