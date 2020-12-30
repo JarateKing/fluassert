7babb9dc4f6bcfd2c811d2a8fbf4ce2a47b2e436
JarateKing <scmacintosh@upei.ca>
2020-12-30 14:00:13 -0400

    added option for getting output from run

--- doc/gen_examples.py
+++ doc/gen_examples.py
@@ -1,3 +1,5 @@
+import subprocess
+
 toprint = []
 with open('examples.mdsource', 'r') as file:
 	for line in file.readlines():
@@ -6,6 +8,11 @@ with open('examples.mdsource', 'r') as file:
 			with open(words[1], 'r') as subfile:
 				for subline in subfile.readlines():
 					toprint.append(subline)
+		elif (len(words) > 0 and words[0] == '@@output'):
+			cmd = subprocess.run(line[8:], shell=True)
+			cmd = subprocess.run('a.exe', shell=True, capture_output=True)
+			output = cmd.stderr
+			toprint.append(output.decode())
 		else:
 			toprint.append(line)
 
